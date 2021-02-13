4c991beb09ed65dc72279bbc419fb7320a857515
Catalin <catalin.constantinescu06@yahoo.com>
2021-02-13 18:58:29 +0200

    navbar and deploy

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0Gh9HoICAL2xn95bpnAEsLTt9VCh92ls",
+    authDomain: "netflix-cloneccb.firebaseapp.com",
+    projectId: "netflix-cloneccb",
+    storageBucket: "netflix-cloneccb.appspot.com",
+    messagingSenderId: "515547786825",
+    appId: "1:515547786825:web:59f6102263c09d8f781998",
+    measurementId: "G-05DW6QXP77"
+  };
--- src/Row.js
+++ src/Row.js
@@ -1,42 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
-import './Row.css'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import Youtube from "react-youtube";
+// import movieTrailer from "movie-trailer";
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
 
-const baseImgUrl = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-function Row({ title ,fetchUrl,isLargeRow }) {
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            
-            return request;
-        }
-        fetchData(); 
-        },[fetchUrl]);
-        
-    
-    return (
-    <div className='row'>
-        <h1>{ title }</h1>
-        
-        <div className ='row_posters'>
-            {movies.map(movie => (
-            
-                <img 
-                key={movie.id}
+  // Options for react-youtube
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      const nameForSearch =
+        movie?.name ||
+        movie?.title ||
+        movie?.original_name ||
+        movie?.original_title ||
+        "";
+
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      };
+      const query = `${nameForSearch}officialTrailer`;
+
+      fetch(
+        `https://www.googleapis.com/youtube/v3/search?q=${query}&key=AIzaSyCW6ykNDFf0_u6mLKFFbAwxBvzje1hrqdo`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setTrailerUrl(result.items[0].id.videoId);
+        })
+
+        .catch((error) => console.log("error", error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map(
+          (movie) =>
+            movie.backdrop_path !== null && (
+              <img
                 className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                
-                src={`${baseImgUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                alt = {movie.name}
-                />
-            
-            ))}
-        </div>
+                src={`${baseImgUrl}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                key={movie.id}
+                onClick={() => handleClick(movie)}
+              />
+            )
+        )}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
-    );
+  );
 }
 
-export default Row
+export default Row;
+
